a6fd48fb8af5c9835bcd0dea85e347b32f7cacbf
konstantinos mazarakis <kostasbusiness@gmail.com>
2022-04-05 02:07:24 -0400

    commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var box = new List<object>();
+            box.Add(7);
+            box.Add(28);
+            box.Add(-1);
+            box.Add(true);
+            box.Add("chair");
+
+            int sum = 0;
+            for(int i = 0; i < box.Count;i++)
+            {
+                Console.WriteLine(box[i]);
+                if(box[i] is int)
+                {
+                    sum = sum + (int)box[i];
+                }
+            }
+                Console.WriteLine(sum);
+
+
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
